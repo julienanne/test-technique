8405d379e3f9a69e13ba368beb2cda5091b2d42c
Julien ANNE <contact@julien-anne.fr>
2021-11-05 10:40:08 +0100

    Feat raw show pages for shop

--- spec/requests/shops_spec.rb
+++ spec/requests/shops_spec.rb
@@ -13,6 +13,90 @@ RSpec.describe "Shops", type: :request do
         get "/shops/shop1"
         expect(@controller.instance_variable_get(:@shop)).to eq(shop)
       end
+
+      context "with taxons existing" do
+        let!(:taxons) { [Taxon.create(name: "taxon1"), Taxon.create(name: "taxon2"), Taxon.create(name: "taxon3")] }        
+        context "with products in taxons existing" do
+          it "returns the taxons whith shop products in" do
+            taxon1 = Taxon.first
+            
+            product1 = Product.create(name: "Product1")
+            product1.shop = shop
+            product1.taxons = [taxon1]
+            product1.save
+            
+            get "/shops/shop1"
+            expect(@controller.instance_variable_get(:@taxons_products)).to eq({taxon1 => [product1]})
+          end
+
+          it "returns the taxons whith shop products in without double taxons" do
+            taxon1 = Taxon.first
+            taxon2 = Taxon.all[1]            
+            
+            product1 = Product.create(name: "Product1")
+            product1.shop = shop
+            product1.taxons = [taxon1]
+            product1.save
+
+            product2 = Product.create(name: "Product2")
+            product2.shop = shop
+            product2.taxons = [taxon1]
+            product2.save
+
+            product3 = Product.create(name: "Product3")
+            product3.shop = shop
+            product3.taxons = [taxon2]
+            product3.save
+
+            product4 = Product.create(name: "Product4")
+            product4.shop = shop
+            product4.taxons = [taxon2]
+            product4.save           
+            
+            get "/shops/shop1"
+            
+            expected_taxons_products = { taxon1 => [product1, product2], taxon2 => [product3, product4] }
+            expect(@controller.instance_variable_get(:@taxons_products)).to eq(expected_taxons_products)
+          end
+
+          it "returns the taxons whith shop products in without double taxons, products in multi taxons" do
+            taxon1 = Taxon.first
+            taxon2 = Taxon.all[1]            
+            
+            product1 = Product.create(name: "Product1")
+            product1.shop = shop
+            product1.taxons = [taxon1]
+            product1.save
+
+            product2 = Product.create(name: "Product2")
+            product2.shop = shop
+            product2.taxons = [taxon1, taxon2]
+            product2.save
+
+            product3 = Product.create(name: "Product3")
+            product3.shop = shop
+            product3.taxons = [taxon2]
+            product3.save
+
+            product4 = Product.create(name: "Product4")
+            product4.shop = shop
+            product4.taxons = [taxon1, taxon2]
+            product4.save           
+            
+            get "/shops/shop1"
+            
+            expected_taxons_products = { taxon1 => [product1, product2, product4], taxon2 => [product2, product3, product4] }
+            expect(@controller.instance_variable_get(:@taxons_products)).to eq(expected_taxons_products)
+          end
+        end
+
+        context "without product in taxons existing" do
+          it "returns the taxons whith shop products in" do                                 
+            get "/shops/shop1"
+            expect(@controller.instance_variable_get(:@taxons_products)).to be_empty
+          end
+        end
+      end
     end
 
     context "with an existing and unavailable shop" do
--- spec/views/shops/show.html.erb_spec.rb
+++ spec/views/shops/show.html.erb_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
-# RSpec.describe "shops/show.html.erb", type: :view do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe "shops/show.html.erb", type: :view do
+  it "displays the name of the shop" do
+    assign(:shop, Shop.create(name: "shop1"))
+
+    expected_taxons_products = { Taxon.new(name: "Taxon1") => [Product.new(name: "Product1"), Product.new(name: "Product2")], Taxon.new(name: "Taxon2") => [Product.new(name: "Product3"), Product.new(name: "Product4")] }
+    assign(:taxons_products, expected_taxons_products)
+    
+    render
+
+    expect(rendered).to match /shop1/
+  end
+
+  it "displays the taxons_products part of the shop" do
+    assign(:shop, Shop.create(name: "shop1"))     
+    
+    expected_taxons_products = { Taxon.new(name: "Taxon1") => [Product.new(name: "Product1"), Product.new(name: "Product2")], Taxon.new(name: "Taxon2") => [Product.new(name: "Product3"), Product.new(name: "Product4")] }
+    assign(:taxons_products, expected_taxons_products)
+    
+    render
+
+    expect(rendered).to match /Taxon1/
+    expect(rendered).to match /Taxon2/
+
+    expect(rendered).to match /Product1/
+    expect(rendered).to match /Product2/
+    expect(rendered).to match /Product3/
+    expect(rendered).to match /Product4/
+  end
+
+  it "displays a message if no products in the shop" do
+    assign(:shop, Shop.create(name: "shop1"))
+    assign(:taxons_products, {})
+    
+    render
+
+    expect(rendered).to match /Aucun produit pour le moment./
+  end
+end
