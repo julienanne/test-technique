7482a74e097dfb0e947bbdb0490199dd492b7f9d
Julien ANNE <contact@julien-anne.fr>
2021-11-05 10:02:13 +0100

    Get the taxons and associated products in controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,15 @@
 class ShopsController < ApplicationController
   def show
     @shop = Shop.available.find_by_slug!(params[:slug])
+    @products = @shop.products.includes(:taxons)
+    @taxons_products = {}
+    @products.each do |product|
+      product.taxons.each do |taxon|
+        if !@taxons_products.has_key?(taxon)        
+          @taxons_products[taxon] = []
+        end
+        @taxons_products[taxon] << product
+      end
+    end
   end
 end
